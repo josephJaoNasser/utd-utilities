ceb637b451acd0fb780c4bde21fd72741e05f265
Joseph Nasser <57519561+josephJaoNasser@users.noreply.github.com>
2024-01-26 14:36:04 +0800

    refactor: style for AI image builder

--- src/components/PhotoUtilities/AIArtCreator/AIArtCreator.vue
+++ src/components/PhotoUtilities/AIArtCreator/AIArtCreator.vue
@@ -2,8 +2,19 @@
   <div>
     <h2>Create AI Art</h2>
     <p>Create AI Art with our free AI image generator.</p>
-    <UTDInput v-model="prompt" placeholder="What do you want to create?" />
-    <UTDButton class="mt-2" @click="getAIArt">Generate Images</UTDButton>
+    <b-row>
+      <b-col cols="12" lg="7" class="mb-2">
+        <UTDInput v-model="prompt" placeholder="What do you want to create?" />
+      </b-col>
+      <b-col cols="12" lg="5">
+        <UTDButton class="mr-2 mb-2" @click="getAIArt"
+          >Generate Images</UTDButton
+        >
+        <UTDButton class="mr-2 mb-2" @click="getAIArt(true)"
+          >Create random image</UTDButton
+        >
+      </b-col>
+    </b-row>
     <div v-if="imageGenMarkup.length">
       <iframe class="h-100 w-100 border-0" :srcdoc="imageGenMarkup"></iframe>
     </div>
@@ -27,9 +38,15 @@ export default {
     };
   },
   methods: {
-    async getAIArt() {
+    async getAIArt(random) {
+      let prompt = this.prompt;
+
+      if (random) {
+        prompt = "Create a random image";
+      }
+
       try {
-        const res = await UTDService.generateAIImages(this.prompt, {
+        const res = await UTDService.generateAIImages(prompt, {
           token: this.token,
           accountId: 11,
         });
