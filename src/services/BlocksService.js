9bb16afe1ce22048c9786da39e6543f1a0948943
Joseph Nasser <57519561+josephJaoNasser@users.noreply.github.com>
2024-03-08 18:24:12 +0800

    add api call functions to block service

--- src/services/BlocksService.js
+++ src/services/BlocksService.js
@@ -1,5 +1,41 @@
+import UTDService from "./UTDService";
 class BlockService extends UTDService {
   constructor(token) {
     super(token);
   }
+
+  /**
+   * Gets the block data
+   * @returns
+   */
+  async getBlocks() {
+    try {
+      const { data } = await this.axiosInstance.get(
+        `https://www.uptodateconnect.com/api/v1/site-builder/blocks?version=2&developer=true`
+      );
+
+      return data;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  /**
+   * Gets the HTML Code for a block
+   * @param {string | number} blockId
+   * @returns
+   */
+  async getBlockCode(blockId) {
+    try {
+      const { data } = await this.axiosInstance.get(
+        `https://www.uptodateconnect.com/api/v1/site-builder/blocks/${blockId}`
+      );
+
+      return data;
+    } catch (e) {
+      throw e;
+    }
+  }
 }
+
+export default BlockService
